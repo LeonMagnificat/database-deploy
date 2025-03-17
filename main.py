e35091982de39149aa0d8bba73f41a88ed2da224
Samuel Dushime <127239702+Samenergy@users.noreply.github.com>
2025-03-18 01:35:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,6 @@ app.include_router(person_router, tags=["CRUD Operations in MYSQL"])
 @app.get("/")
 async def root():
     return {"message": "MySQL with FASTAPI is running!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
